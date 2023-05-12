9d3e00844303d7b635bd9d691a30a02884706797
Ionut Spalatelu <ionut.spalatelu@vdx.tv>
2023-05-12 10:36:46 +0300

    Update comment

--- src/main/java/com/ibm/_1_intro/FlowStatements.java
+++ src/main/java/com/ibm/_1_intro/FlowStatements.java
@@ -6,7 +6,6 @@ public class FlowStatements {
 	
 	public static void main(String[] args) {
 		if(args.length > 0){
-//			String arg = args[0];
 			String arg = args[0];
 			System.out.println("arg = " + arg);
 			System.out.println("arg 2 = " + args[1]);
