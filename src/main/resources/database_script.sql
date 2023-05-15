d1dd485909a2392d0dcc1c9f5b67d07d94acf530
Ionut Spalatelu <ionutspalatelu@gmail.com>
2023-05-15 11:04:31 +0300

    SQL script

--- src/main/resources/database_script.sql
+++ src/main/resources/database_script.sql
@@ -1,1 +1,51 @@
+--// Comanda pentru a porni MySQL local cu Docker
+--// docker run -d -p 3306:3306 --name mysql-docker-container -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=spotify -e MYSQL_USER=my-user -e MYSQL_PASSWORD=secret mysql/mysql-server:latest
+
+drop database if exists spotify;
+create database spotify;
+use spotify;
+
+create table singer
+(
+    id        bigint auto_increment primary key,
+    firstName varchar(50) null,
+    lastName  varchar(50) not null,
+    birthDate date        null
+);
+
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (1, 'John', 'Lennon', '1940-10-09');
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (2, 'Ringo', 'Starr', '1940-07-07');
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (3, 'Pete', 'Best', '1941-11-24');
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (4, 'Elvis', 'Presley', '1935-01-08');
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (5, 'Michael', 'Jackson', '1958-08-29');
+INSERT INTO spotify.singer (id, firstName, lastName, birthDate)
+VALUES (6, 'John', 'Doe', '1940-10-09');
+
+create table album
+(
+    id          bigint auto_increment primary key,
+    singerId    bigint       null,
+    name        varchar(50)  not null,
+    releaseDate date         not null,
+    description varchar(200) null,
+    constraint album_singer_id_fk
+        foreign key (singerId) references singer (id)
+            on update cascade on delete cascade
+);
+
+INSERT INTO spotify.album (id, singerId, name, releaseDate, description)
+VALUES (1, 1, 'The Beatles', '1968-01-01', 'Beatles album');
+INSERT INTO spotify.album (id, singerId, name, releaseDate, description)
+VALUES (2, 2, 'The Beatles', '1968-01-01', 'Beatles album');
+INSERT INTO spotify.album (id, singerId, name, releaseDate, description)
+VALUES (3, 3, 'The Beatles', '1968-01-01', 'Beatles album');
+INSERT INTO spotify.album (id, singerId, name, releaseDate, description)
+VALUES (4, 5, 'Thriller', '1982-01-01', 'Best sold all time');
+INSERT INTO spotify.album (id, singerId, name, releaseDate, description)
+VALUES (5, 4, 'Heartbreak Hotel', '1956-01-05', 'Best Elvis song');
 
